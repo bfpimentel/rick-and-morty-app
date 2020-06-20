b88c6f796e6f2172f2384bbdc781020aa64e5d7b
Bruno Pimentel <bruno@pimentel.dev>
2020-06-20 19:14:36 -0300

    feat: adds timber to the project

--- app/src/main/java/dev/pimentel/rickandmorty/RickAndMortyApplication.kt
+++ app/src/main/java/dev/pimentel/rickandmorty/RickAndMortyApplication.kt
@@ -2,10 +2,9 @@ package dev.pimentel.rickandmorty
 
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
+import timber.log.Timber
 
 @Suppress("Unused")
 class RickAndMortyApplication : Application() {
@@ -14,8 +13,8 @@ class RickAndMortyApplication : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@RickAndMortyApplication)
-            androidLogger(Level.DEBUG)
             loadKoinModules(appModules)
         }
+        Timber.plant(Timber.DebugTree())
     }
 }
